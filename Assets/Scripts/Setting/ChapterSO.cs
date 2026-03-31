91ab7fe5e211b57bdd861d543305f93a9597247c
jyjunPepe012233 <jinyj0217@gmail.com>
2026-03-31 19:55:34 +0900

    update chapter and stage data relation

--- Assets/Scripts/Setting/ChapterSO.cs
+++ Assets/Scripts/Setting/ChapterSO.cs
@@ -5,4 +5,6 @@ public class ChapterSO : ScriptableObject
 {
 	[SerializeField] private string _chapterName;
 	public string ChapterName => _chapterName;
+
+	[SerializeField] private StageSO[] _stages;
 }
--- Assets/Scripts/Setting/StageSO.cs
+++ Assets/Scripts/Setting/StageSO.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Stage")]
 public class StageSO : ScriptableObject
 {
-	[SerializeField] private ChapterSO _chapter;
-	public ChapterSO Chapter => _chapter;
-	
 	[SerializeField] private string _stageName;
 	public string StageName => _stageName;
 }
