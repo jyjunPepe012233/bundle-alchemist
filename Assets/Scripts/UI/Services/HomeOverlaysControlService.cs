fcf90498f4f3ee097c49468d51bf86f0833de0f9
jyjunPepe012233 <jinyj0217@gmail.com>
2026-04-17 00:33:54 +0900

    add home overlays control service

---
+++ Assets/Scripts/UI/Services/HomeOverlaysControlService.cs
@@ -0,0 +1,20 @@
+using ProjectB.UI.Screens.Home;
+using UnityEngine;
+
+namespace ProjectB.UI.Services
+{
+
+	public class HomeOverlaysControlService : MonoBehaviour
+	{
+		public void OpenOverlay(string overlayId)
+		{
+			HomeOverlaysController.OpenOverlay?.Invoke(overlayId);
+		}
+		
+		public void CloseOverlay()
+		{
+			HomeOverlaysController.CloseOverlay?.Invoke();
+		}
+	}
+
+}
