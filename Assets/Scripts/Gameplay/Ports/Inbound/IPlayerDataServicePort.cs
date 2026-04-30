d383f919e25e391921458e5720c1303a6b552bb7
jyjunPepe012233 <jinyj0217@gmail.com>
2026-04-30 15:35:41 +0900

    add note

--- Assets/Scripts/Gameplay/Ports/Inbound/IPlayerDataServicePort.cs
+++ Assets/Scripts/Gameplay/Ports/Inbound/IPlayerDataServicePort.cs
@@ -5,6 +5,8 @@ namespace ProjectB.Gameplay.Ports.Inbound
 
 	public interface IPlayerDataServicePort
 	{ 
+		// UI 등의 외부 시스템에서 PlayerData를 조작할 수 없도록
+		// PlayerData를 IReadOnlyPlayerData로 추상화해서 제공하였음
 		IReadOnlyPlayerData GetPlayerData();
 	}
 
