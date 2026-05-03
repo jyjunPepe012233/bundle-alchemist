759a4935beca19e8185b148c5013e9b09792baa9
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-03 16:48:05 +0900

    add note

--- Assets/Scripts/Data/Runtime/Player/IReadOnlyPlayerSoldier.cs
+++ Assets/Scripts/Data/Runtime/Player/IReadOnlyPlayerSoldier.cs
@@ -9,7 +9,7 @@ namespace ProjectB.Data.Runtime.Player
 		
 		int Exp { get; }
 		
-		short Level { get; } // TODO: 그냥 int로 바꾸기. 메모리 2byte 줄여봐야 의미 없음
+		short Level { get; } // TODO: 그냥 int로 바꾸기. 메모리 2byte 줄여봐야 의미 없음 (메서드 호환성 안 좋음)
 
 		event Action ExpChanged;
 
