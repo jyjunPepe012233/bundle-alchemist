be4f399009a5d10fbf5e64c1f67f4405ad3671b8
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-06 09:04:26 +0900

    add note on IPlayerSoldierFactory

--- Assets/Scripts/Gameplay/Ports/Internal/IPlayerSoldierFactory.cs
+++ Assets/Scripts/Gameplay/Ports/Internal/IPlayerSoldierFactory.cs
@@ -4,7 +4,7 @@ using ProjectB.Data.Static.Soldier;
 namespace ProjectB.Gameplay.Ports.Internal
 {
 
-	public interface IPlayerSoldierFactory
+	public interface IPlayerSoldierFactory // TODO: 이름 뒤에 Port 붙이기
 	{
 		IPlayerSoldier Create(ISoldierData soldierData);
 	}
