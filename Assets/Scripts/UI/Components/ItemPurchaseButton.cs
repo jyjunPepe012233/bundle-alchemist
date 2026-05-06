d056feb2b276df65c62453d392326e31bc3dfa90
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-06 10:10:31 +0900

    add item purchase button ui component

---
+++ Assets/Scripts/UI/Components/ItemPurchaseButton.cs
@@ -0,0 +1,52 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjectB.UI.Components
+{
+
+	public class ItemPurchaseButton : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI _itemNameText;
+		[SerializeField] private TextMeshProUGUI _itemQuantityText;
+		[SerializeField] private TextMeshProUGUI _priceText;
+		[SerializeField] private Button _purchaseButton;
+		[SerializeField] private Transform _backgroundParent;
+
+		public event Action PurchaseButtonClicked;
+		
+		private GameObject _backgroundInstance;
+		
+		public void SetItemNameTex(string text)
+		{
+			_itemNameText.text = text;
+		}
+		
+		public void SetItemQuantityText(string text)
+		{
+			_itemQuantityText.text = text;
+		}
+		
+		public void SetPriceText(string text)
+		{
+			_priceText.text = text;
+		}
+		
+		public void SetPurchaseButtonInteractable(bool interactable)
+		{
+			_purchaseButton.interactable = interactable;
+		}
+
+		public void SetBackground(GameObject prefab)
+		{
+			if (_backgroundInstance != null)
+			{
+				Destroy(_backgroundInstance);
+			}
+			
+			_backgroundInstance = Instantiate(prefab, _backgroundParent);
+		}
+	}
+
+}
